5a2e94381b585af47b9e411aec3726763d08dd48
razzzu <raj.patel8396@gmail.com>
2020-04-24 20:29:48 -0700

    psi empty module added

---
+++ psi.v
@@ -0,0 +1,10 @@
+//compile using: vcs -F vcs-psi-compile.txt psi.v
+
+module psi #(parameter DSIZE=32, ASIZE=32) (output s_data, grant, ready,
+                                            input [DSIZE-1:0] data,      
+                                            input req, pkt_end, p_clk, s_clk, n_rst);
+
+    
+
+
+endmodule
