904d298031e5e4913ed96e764fb473e9086ae201
razzzu <raj.patel8396@gmail.com>
2020-04-24 20:28:54 -0700

    serial_com top module added

---
+++ serial_com.v
@@ -0,0 +1,5 @@
+module serial_com #(parameter DSIZE=32) (output s_out, r_en
+                                input [DSIZE-1:1] rdata,
+                                input rempty, rclk, rrst_n);
+
+endmodule
