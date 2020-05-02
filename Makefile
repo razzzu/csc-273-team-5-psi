374df578cb0f68b412697a99988dd5f1e386c6f3
Ethan Weidman <weidman@csus.edu>
2020-05-02 00:51:16 -0700

    Merge conficts resolved.
    Added serial_com and pi to Makefile.

--- Makefile
+++ Makefile
@@ -12,6 +12,8 @@ sync: sim/sync_test
 
 pi: sim/pi_test
 
+serial_com: sim/serial_com
+
 sim/fifo: design/fifo/fifo.v testbench/simple_fifo_fixture.v
 	vcs -full64 +lint=all -o sim/fifo +incdir+./design/fifo/ testbench/fifo_fixture.v
 
@@ -23,6 +25,9 @@ sim/sync_test: design/experiments/sync_ptr_2_clk.v design/fifo/sync_ptr.v testbe
 
 sim/pi_test: design/pi/com_fsm.v design/pi/par_com.v design/dma_beh/dma_beh.v design/fifo/fifo.v testbench/pi_fixture.v
 	vcs -debug_access -full64 -o sim/pi_test +incdir+./design/pi/+./design/dma_beh/+./design/fifo/ testbench/pi_fixture.v
+
+sim/serial_com: design/si/serial_com.v testbench/serial_com_fixture.v
+	vcs -full64 +lint=all -debug_access -o sim/serial_com +incdir+./design/si/ testbench/serial_com_fixture.v
 
 clean:
 	find sim -mindepth 1 ! -name .gitignore -delete
--- design/si/serial_com.v
+++ design/si/serial_com.v
@@ -1,5 +1,82 @@
-module serial_com #(parameter DSIZE=32) (output s_out, r_en
-                                input [DSIZE-1:1] rdata,
+module serial_com #(parameter DSIZE=32) (output s_out, r_en,
+                                input [DSIZE:0] rdata,
                                 input rempty, rclk, rrst_n);
+
+reg s_out, r_en;
+reg [3:0] state;
+reg [7:0] bit_counter;
+reg [16:1] shift_register;
+
+always @(posedge rclk or negedge rrst_n)
+begin
+    if (!rrst_n)
+    begin
+        state <= 4'b0;
+        bit_counter <= 8'b11111111;
+        shift_register <= 16'b01110;
+    end
+    else
+    begin
+        if (state == 4'b0000)
+        begin
+            bit_counter <= 8'b11111111;
+        end
+        else
+        begin
+            if (bit_counter[3:0] == 4'b1111)
+            begin
+                if (state == 4'b0001)
+                    shift_register[16:1] <= 16'h5a5a;
+                else if (state == 4'b0010 && bit_counter[4])
+                    shift_register[16:1] <= rdata[31:16];
+                else if (state == 4'b0010)
+                    shift_register[16:1] <= rdata[15:0];
+                else if (state == 4'b0011)
+                    shift_register[16:1] <= 16'h0f0f;
+                else
+                    shift_register[16:1] <= 16'h1234;
+            end
+            else
+                shift_register <= shift_register >> 1;
+            bit_counter <= bit_counter + 1;
+        end
+    end
+end
+
+always @(*)
+begin
+    s_out = shift_register[1]; r_en = 1'b0;
+    case (state)
+        4'b0000:
+            if (!rempty) state = 4'b0001;
+        4'b0001:
+            if (bit_counter[5:4] == 2'b10)
+            begin
+                state = 4'b0010;
+                bit_counter = 8'b11111111;
+            end
+        4'b0010:
+            if (bit_counter[5:4] == 2'b10 && rdata[32])
+            begin
+                state = 4'b0011;
+                bit_counter = 8'b11111111;
+            end
+            else if (bit_counter[5:4] == 2'b10)
+            begin
+                r_en = 1'b1;
+            end
+        4'b0011:
+            if (bit_counter[6:4] == 3'b100 && !rempty)
+            begin
+                state = 4'b0001;
+                bit_counter = 8'b11111111;
+            end
+            else if (bit_counter[6:4] == 3'b100)
+            begin
+                state = 4'b0000;
+                bit_counter = 8'b11111111;
+            end
+    endcase
+end
 
 endmodule
